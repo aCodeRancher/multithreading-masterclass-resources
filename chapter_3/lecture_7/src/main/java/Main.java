d0002ff3db5992681709fcbf561a880852754f7b
helen <hhma@alumni.cpp.edu>
2022-02-13 22:36:16 -0500

    remove unused imports.

--- chapter_3/lecture_7/src/main/java/Main.java
+++ chapter_3/lecture_7/src/main/java/Main.java
@@ -1,7 +1,4 @@
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Main {
 
     private static Semaphore semaphore = new Semaphore(2);
