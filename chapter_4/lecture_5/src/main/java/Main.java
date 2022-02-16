e987f7a7a5625edbf46937e50395a5808fb14c4c
helen <hhma@alumni.cpp.edu>
2022-02-15 22:44:06 -0500

    create a live lock example.

--- chapter_4/lecture_5/src/main/java/Main.java
+++ chapter_4/lecture_5/src/main/java/Main.java
@@ -1,35 +1,50 @@
+import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
-    private static Lock lock1 = new ReentrantLock();
-    private static Lock lock2 = new ReentrantLock();
-
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> {
-            lock2.tryLock();
-            System.out.println("Thread 1 acquired lock2");
 
-            lock1.lock();
-            System.out.println("Thread 1 acquired lock1");
-            lock1.unlock();
-
-            lock2.unlock();
-        });
-
-        Thread t2 = new Thread(() -> {
-            lock2.lock();
-            System.out.println("Thread 2 acquired lock2");
-
-            lock1.lock();
-            System.out.println("Thread 2 acquired lock1");
-            lock1.unlock();
-
-            lock2.unlock();
-        });
-
+        ReentrantLock lock1 = new ReentrantLock();
+        ReentrantLock lock2 = new ReentrantLock();
+        Thread t1 = new Thread(new Player(lock1, lock2));
+        Thread t2 = new Thread(new Player(lock2, lock1));
         t1.start();
         t2.start();
+
     }
 }
+    class Player implements Runnable{
+        ReentrantLock locka;
+        ReentrantLock lockb;
+
+        public Player(ReentrantLock lock1, ReentrantLock lock2){
+             this.locka = lock1;
+             this.lockb = lock2;
+        }
+
+        @Override
+        public void run(){
+            Random rand = new Random();
+            int interval = 1000;
+            locka.lock();
+            System.out.println(Thread.currentThread().getName() + " gain its lock.");
+            while (lockb.isLocked()) {
+                System.out.println(Thread.currentThread().getName() + " release its lock to step aside.");
+                locka.unlock();
+                try {
+                    Thread.sleep(rand.nextInt(interval));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " gain its lock again.");
+                locka.lock();
+
+            }
+            System.out.println(Thread.currentThread().getName() + " release another lock if it is locked.");
+            if (lockb.isLocked())
+                 lockb.unlock();
+        }
+    }
+
