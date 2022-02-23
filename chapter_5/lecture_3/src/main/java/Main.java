23953180c1e77c40b24ec1463eb2617fbcdaeed7
helen <hhma@alumni.cpp.edu>
2022-02-23 15:53:33 -0500

    add id for each task.

--- chapter_5/lecture_3/src/main/java/Main.java
+++ chapter_5/lecture_3/src/main/java/Main.java
@@ -11,17 +11,26 @@ public class Main {
                  new ArrayBlockingQueue<>(3)
          );
 
-       for (int i=0;i<20;i++)
-           threadPoolExecutor.submit( new SleepingTask(i));
+       for (int i=0;i<8;i++) {
+           SleepingTask task = new SleepingTask(i);
+           threadPoolExecutor.submit(task);
+           System.out.println("Thread " + task.getId()  + " Thread pool size: " + threadPoolExecutor.getPoolSize());
+           System.out.println("Thread " + task.getId()  + " Active count: " + threadPoolExecutor.getActiveCount());
+           System.out.println("Thread " + task.getId() + " task count: " + threadPoolExecutor.getTaskCount());
 
+       }
        threadPoolExecutor.shutdown();
        threadPoolExecutor.awaitTermination(1 , TimeUnit.MINUTES);
+        System.out.println( "completed task count: "+ threadPoolExecutor.getCompletedTaskCount());
     }
 
     static class SleepingTask implements Runnable{
          private final int id;
          public SleepingTask (int id) { this.id= id;}
 
+         public int getId() {
+           return id;
+         }
          @Override
          public void run(){
              try{
