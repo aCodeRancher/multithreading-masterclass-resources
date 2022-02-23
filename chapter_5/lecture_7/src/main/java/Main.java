06dfafb8d89b5aa479608a8aff92d0a2a89f794f
helen <hhma@alumni.cpp.edu>
2022-02-22 20:34:35 -0500

    experiment with 50K threads with the set policy

--- chapter_5/lecture_7/src/main/java/Main.java
+++ chapter_5/lecture_7/src/main/java/Main.java
@@ -3,16 +3,45 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
-        ScheduledThreadPoolExecutor threadPoolExecutor = new ScheduledThreadPoolExecutor(3);
+    public static void main(String[] args) throws InterruptedException{
+        ScheduledThreadPoolExecutor threadPoolExecutor = new ScheduledThreadPoolExecutor(4);
+       // threadPoolExecutor.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+        threadPoolExecutor.setContinueExistingPeriodicTasksAfterShutdownPolicy(false);
+       //  threadPoolExecutor.schedule(() -> System.out.println("Task"), 5, TimeUnit.SECONDS);
+        for (int i=0;i<50000;i++) {
+            ScheduledFuture<?> future = threadPoolExecutor.schedule(new SleepingTask(i), 5, TimeUnit.SECONDS);
 
-//        threadPoolExecutor.schedule(() -> System.out.println("Task"), 5, TimeUnit.SECONDS);
-        ScheduledFuture<?> future = threadPoolExecutor.scheduleAtFixedRate(() -> System.out.println("Task"), 5, 1, TimeUnit.SECONDS);
+        }
 
-        threadPoolExecutor.shutdown();
+        // threadPoolExecutor.shutdown();
+        threadPoolExecutor.awaitTermination(20, TimeUnit.SECONDS);
+        threadPoolExecutor.shutdown ();
+        System.out.println("shutting down");
+
+        Thread.sleep(30000);
+
+        //threadPoolExecutor.awaitTermination(50, TimeUnit.SECONDS);
         threadPoolExecutor.shutdownNow();
-        threadPoolExecutor.setExecuteExistingDelayedTasksAfterShutdownPolicy(true);
-        threadPoolExecutor.setContinueExistingPeriodicTasksAfterShutdownPolicy(true);
+        System.out.println("Shutting down now");
 
+    //   threadPoolExecutor.setContinueExistingPeriodicTasksAfterShutdownPolicy(true);
+
+    }
+
+    static class SleepingTask implements Runnable{
+
+        private int id;
+
+        public SleepingTask(int id){
+            this.id= id;
+        }
+        @Override
+        public void run(){
+            try{
+                System.out.println("Thread : " + id + " Task and then wait for a while");
+                Thread.sleep(5000);
+            }
+            catch (InterruptedException e){}
+        }
     }
 }
