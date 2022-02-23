6f59022c5655a8fac7273f3070e9269bd70d4ec9
helen <hhma@alumni.cpp.edu>
2022-02-23 15:52:28 -0500

    add discardoldestpolicy

--- chapter_5/lecture_5/src/main/java/Main.java
+++ chapter_5/lecture_5/src/main/java/Main.java
@@ -3,24 +3,28 @@ import java.util.concurrent.*;
 public class Main {
     public static void main(String[] args) {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
-                2,
-                3,
+                1,
+                1,
                 1,
                 TimeUnit.MINUTES,
                 new ArrayBlockingQueue<>(2),
-                new ThreadPoolExecutor.DiscardPolicy()
-        );
+                new ThreadPoolExecutor.DiscardOldestPolicy()
+
+         );
 
         threadPoolExecutor.submit(new SleepingTask(1));
         threadPoolExecutor.submit(new SleepingTask(2));
 
         System.out.println("[1] Pool size: " + threadPoolExecutor.getPoolSize());
 
+        //These two tasks are in the queue waiting.
         threadPoolExecutor.submit(new SleepingTask(3));
         threadPoolExecutor.submit(new SleepingTask(4));
 
+        //create one more thread as the queue is full now.
         threadPoolExecutor.submit(new SleepingTask(5));
         System.out.println("[2] Pool size: " + threadPoolExecutor.getPoolSize());
+        //queue is still full and no more thread can be created. It is rejected.
 
         threadPoolExecutor.submit(new SleepingTask(6));
     }
@@ -36,7 +40,8 @@ public class Main {
         @Override
         public void run() {
             try {
-                Thread.sleep(99999);
+                Thread.sleep(70000);
+                System.out.println("Thread " + id  + " runs.");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
