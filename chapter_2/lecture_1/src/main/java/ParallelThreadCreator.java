920b7cd9f4a9dd80bdd96b1eb097da3f116066e6
helen <hhma@alumni.cpp.edu>
2022-03-07 20:25:45 -0500

    Use executor service.

--- chapter_2/lecture_1/src/main/java/ParallelThreadCreator.java
+++ chapter_2/lecture_1/src/main/java/ParallelThreadCreator.java
@@ -1,32 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ParallelThreadCreator {
     // creating 10 threads and waiting for them to complete then again repeat steps.
     public static void multiply(int[][] matrix1, int[][] matrix2, int[][] result) {
-        List<Thread> threads = new ArrayList<>();
+        //This machine has 8 CPU cores.
+        int parallelism = 8;
+        ExecutorService threadPool = Executors.newFixedThreadPool(parallelism);
         int rows1 = matrix1.length;
         for (int i = 0; i < rows1; i++) {
             for (int j=0;j<matrix2[0].length;j++) {
                 RowMultiplyWorker task = new RowMultiplyWorker(result, matrix1, matrix2, i, j);
-                Thread thread = new Thread(task);
-                thread.start();
-                threads.add(thread);
-                if (threads.size() % 10 == 0) {
-                    waitForThreads(threads);
-                }
+                threadPool.submit(task);
             }
         }
+        threadPool.shutdown();
     }
 
-    private static void waitForThreads(List<Thread> threads) {
-        for (Thread thread : threads) {
-            try {
-
-                 thread.join();
-             } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-        }
-
-    }
 }
