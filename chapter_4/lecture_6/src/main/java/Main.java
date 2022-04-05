8d938a9ee0fa87cd867c66c2f5825a02ec72eacb
helen <hhma@alumni.cpp.edu>
2022-04-05 19:46:38 -0400

    use atomic integer

--- chapter_4/lecture_6/src/main/java/Main.java
+++ chapter_4/lecture_6/src/main/java/Main.java
@@ -1,12 +1,14 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
 
-    private static volatile int counter = 0;
+    private static AtomicInteger counter =new AtomicInteger(0);
 
     public static void main(String[] args) {
         Thread t1 = new Thread(() -> {
-            int local_counter = counter;
+            var local_counter = counter;
 
-            while (local_counter < 10) {
+            while (local_counter.get() < 10) {
                 if (local_counter != counter) {
                     System.out.println("[T1] Local counter is changed ");
                     local_counter = counter;
@@ -15,11 +17,12 @@ public class Main {
         });
 
         Thread t2 = new Thread(() -> {
-            int local_counter = counter;
+           var local_counter = counter;
 
-            while (local_counter < 10) {
-                System.out.println("[T2] Incremented counter to " + (local_counter + 1));
-                counter = ++local_counter;
+            while (local_counter.get() < 10) {
+                System.out.println("[T2] Incremented counter to " + (local_counter.addAndGet(1) ));
+                int newValue =  local_counter.getAndIncrement();
+                counter.set(newValue);
 
                 try {
                     Thread.sleep(500);
